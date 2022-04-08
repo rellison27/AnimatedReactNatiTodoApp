e6a14deda61545e53c6f37a45d53472f6e5ff8a1
Rashaun Ellison <rellison27@gmail.com>
2022-04-08 17:16:55 -0400

    added bouncy checkbox

--- src/components/MainScreen.tsx
+++ src/components/MainScreen.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React, {useState} from 'react'
 import {
   Text,
   Box,
@@ -9,9 +9,14 @@ import {
   useColorMode,
   useColorModeValue,
 } from 'native-base'
+import BouncyCheckbox from 'react-native-bouncy-checkbox'
 import ThemeToggle from './ThemeToggle'
 
 export default function MainScreen() {
+
+  const [isChecked, setIsChecked] = useState<boolean>(false)
+  const handleIsChecked = () => setIsChecked(!isChecked)
+  const { colorMode, toggleColorMode } = useColorMode()
   return (
     <Center
       _dark={{ bg: 'blueGray.900' }}
@@ -23,7 +28,16 @@ export default function MainScreen() {
         <Box p={10} bg={useColorModeValue('red.500', 'yellow.500')}>
           <Text>Hello</Text>
         </Box>
-        <ThemeToggle  />
+        <ThemeToggle />
+        <BouncyCheckbox
+          size={25}
+          fillColor={useColorModeValue('#FF0000', '#FFDB58')}
+          unfillColor="#FFFFFF"
+          text="Custom Checkbox"
+          iconStyle={{ borderColor: useColorModeValue('#FF0000', '#FFDB58') }}
+          textStyle={{ fontFamily: 'JosefinSans-Regular' }}
+          onPress={handleIsChecked}
+        />
       </VStack>
     </Center>
   )
